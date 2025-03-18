a86b27d33f14c8b27326363249f9c94f48d02183
Devadeth shaji <devadethofficial@gmail.com>
2025-03-18 13:53:53 +0530

    Added Railway config

--- avg_erp/settings.py
+++ avg_erp/settings.py
@@ -83,13 +83,12 @@ WSGI_APPLICATION = 'avg_erp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -135,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import django_heroku
 django_heroku.settings(locals())
+
+ALLOWED_HOSTS = ["*"]
